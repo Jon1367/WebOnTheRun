303c20bc3e19b210291934a3c890d0f205c435d0
Jonathan Ortiz <jonathano1367@gmail.com>
2015-09-10 07:14:37 +0000

    firebase data blinding fix

--- client/js/game.js
+++ client/js/game.js
@@ -1,21 +1,19 @@
-var app = angular.module("gameApp", ["firebase"]);
+// create our angular module and inject firebase
+var app = angular.module('gameApp', ['firebase']);
 
-
-app.controller("SampleCtrl", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://destinyapp.firebaseio.com/");
-  // download the data into a local object
-  console.log('Hello');
-  $scope.playerOne = 'asdf';
-  $scope.playerTwo = 'saf';
-  var data = $firebaseObject(ref);
-  data.$loaded().then(function (){
-
+  
+app.controller("SampleCtrl", ["$scope", "$timeout", "$window", function($scope, $timeout, $window) {
+  // create a reference to our Firebase database
+  var ref = new $window.Firebase("https://destinyapp.firebaseio.com/");
+  // read data from the database into a local scope variable
+  ref.on("value", function(snapshot) {
+    // Since this event will occur outside Angular's $apply scope, we need to notify Angular
+    // each time there is an update. This can be done using $scope.$apply or $timeout. We
+    // prefer to use $timeout as it a) does not throw errors and b) ensures all levels of the
+    // scope hierarchy are refreshed (necessary for some directives to see the changes)
+    $timeout(function() {
+      $scope.data = snapshot.val();
       console.log($scope.data);
+    });
   });
-        $scope.data = data;
-});
-
-  
-
-  
-
+}]);
