66191f88954c5359d2e71c5d819b4660e39c1d45
Jonathan Ortiz <jonathano1367@gmail.com>
2015-09-10 02:23:49 +0000

    firebase push

--- client/js/game.js
+++ client/js/game.js
@@ -1,23 +1,21 @@
 var app = angular.module("gameApp", ["firebase"]);
 
-var ref = new Firebase("https://destinyapp.firebaseio.com/");
-// Attach an asynchronous callback to read the data at our posts reference
-ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
 
 app.controller("SampleCtrl", function($scope, $firebaseObject) {
   var ref = new Firebase("https://destinyapp.firebaseio.com/");
   // download the data into a local object
-  $scope.data = $firebaseObject(ref);
-  ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
+  console.log('Hello');
+  $scope.playerOne = 'asdf';
+  $scope.playerTwo = 'saf';
+  var data = $firebaseObject(ref);
+  data.$loaded().then(function (){
+
+      console.log($scope.data);
+  });
+        $scope.data = data;
 });
+
   
-  console.log($scope.data);
-  // putting a console.log here won't work, see below
-});
+
+  
+
